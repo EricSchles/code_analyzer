c9735fec017bc75898b2c1a8ef39068954efff1d
EricSchles <ericschles@gmail.com>
2014-11-08 16:56:37 -0800

    explanation done

--- analyzer.py
+++ analyzer.py
@@ -6,7 +6,12 @@ class PythonAnalyzer:
     self.num_func_calls := number of times a given function is called throughout a piece of code or a project.
     self.num_classes := the number of classes in a piece of code or project
     self.num_class_instances := the number of times the class is instantiated throughout the code base.
+    self.num_linear_algo := number of O(n) code blocks
+    self.num_squared_algo := number of O(n^2) code blocks
+    self.num_cubed_algo := number of O(n^3) code blocks
+    self.num_4th_algo := number of O(n^4) code blocks
     """
+  
     def __init__(self,code,is_project=False):
         if is_project:
             if os.path.isdir(code):
@@ -18,13 +23,18 @@ class PythonAnalyzer:
             else:
                 print "didn't pass in a file, but a file was expected"
                 sys.exit(0)
-            self.ratio = []
-            self.average_num_lines = []
-            self.num_funcs = []
+            #for each - key is file name, value is value of the data
+            self.ratio = {}
+            self.average_num_lines = {}
+            self.num_funcs = {}
             self.num_func_calls = {}
-            self.num_classes = []
+            self.num_classes = {}
             self.num_class_instances = {}
-        else:
+            self.num_linear_algo = {}
+            self.num_squared_algo = {}
+            self.num_cubed_algo = {}
+            self.num_4th_algo = {}
+      else:
             if os.path.isfile(code):
                 self.code = open(code,"r")
             else:
@@ -33,9 +43,13 @@ class PythonAnalyzer:
             self.ratio = 0
             self.average_num_lines = 0
             self.num_funcs = 0
-            self.num_func_calls = {}
+            self.num_func_calls = {} #keys are the functions, values are the calls
             self.num_classes = 0
-            self.num_class_instances = {}
+            self.num_class_instances = {} #keys are the classes, values are the counts
+            self.num_linear_algo = {}
+            self.num_squared_algo = {}
+            self.num_cubed_algo = {}
+            self.num_4th_algo = {}
         self.is_project = is_project
 
     def _ratio(self):
