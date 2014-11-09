caa8aedd8004b1fe1910f471e6ed1d046e783e06
EricSchles <ericschles@gmail.com>
2014-11-08 16:51:35 -0800

    added most of the metrics

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,10 @@ class PythonAnalyzer:
     """
     self.ratio := the number of lines of comments versus the number of lines of code.
     self.average_num_lines := the average number of comments in a row.
-    self.
+    self.num_funcs := the number of functions per file
+    self.num_func_calls := number of times a given function is called throughout a piece of code or a project.
+    self.num_classes := the number of classes in a piece of code or project
+    self.num_class_instances := the number of times the class is instantiated throughout the code base.
     """
     def __init__(self,code,is_project=False):
         if is_project:
@@ -17,6 +20,10 @@ class PythonAnalyzer:
                 sys.exit(0)
             self.ratio = []
             self.average_num_lines = []
+            self.num_funcs = []
+            self.num_func_calls = {}
+            self.num_classes = []
+            self.num_class_instances = {}
         else:
             if os.path.isfile(code):
                 self.code = open(code,"r")
@@ -25,6 +32,10 @@ class PythonAnalyzer:
                 sys.exit(0)
             self.ratio = 0
             self.average_num_lines = 0
+            self.num_funcs = 0
+            self.num_func_calls = {}
+            self.num_classes = 0
+            self.num_class_instances = {}
         self.is_project = is_project
 
     def _ratio(self):
