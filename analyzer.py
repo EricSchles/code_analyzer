3735e1cfa3f94be3db83ec576642435268d850bf
EricSchles <ericschles@gmail.com>
2014-11-08 16:46:27 -0800

    added average number of lines

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,7 @@ class PythonAnalyzer:
     """
     self.ratio := the number of lines of comments versus the number of lines of code.
     self.average_num_lines := the average number of comments in a row.
+    self.
     """
     def __init__(self,code,is_project=False):
         if is_project:
@@ -15,7 +16,7 @@ class PythonAnalyzer:
                 print "didn't pass in a file, but a file was expected"
                 sys.exit(0)
             self.ratio = []
-            
+            self.average_num_lines = []
         else:
             if os.path.isfile(code):
                 self.code = open(code,"r")
@@ -23,6 +24,7 @@ class PythonAnalyzer:
                 print "didn't pass in a file, but file was expected"
                 sys.exit(0)
             self.ratio = 0
+            self.average_num_lines = 0
         self.is_project = is_project
 
     def _ratio(self):
